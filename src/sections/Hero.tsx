d00fe7b1a9ea81cbbe812bf177b53825e1699ab2
Tedros Kahsu <tedkahcom@gmail.com>
2024-09-28 15:22:58 +0300

    Add Project section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -72,7 +72,7 @@ export const HeroSection = () => {
         </div>
         <div className="max-w-lg mx-auto">
           <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
-            Building Exceptional User Expriances
+            Building Exceptional User Experiances
           </h1>
           <p className="mt-4 text-center text-white/60 md:text-lg">
             I specialize in transforming desgins tinto functional,
