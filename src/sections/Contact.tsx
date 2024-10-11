862c462dec74e868c5883b8216df8c0ded070200
Tedros Kahsu <tedkahcom@gmail.com>
2024-10-11 16:52:04 +0300

    Update comma

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -19,10 +19,10 @@ export const ContactSection = () => {
           <div className="flex flex-col md:flex-row gap-8 md:gap-16 items-center ">
             <div>
               <h2 className="font-serif text-2xl md:text-3xl">
-                Let's create something amaizing together
+                Let&apos;s create something amaizing together
               </h2>
               <p className="text-sm mt-2 md:text-base">
-                Ready to bring your next project to life? Let's connect and
+                Ready to bring your next project to life? Let&apos;s connect and
                 discuss I can help you achieve your goals.
               </p>
             </div>
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -126,7 +126,8 @@ export const HeroSection = () => {
           </h1>
           <p className="mt-4 text-center text-white/60 md:text-lg">
             I specialize in transforming desgins tinto functional,
-            high-perfomring web applications. Let's diccuss you next project.
+            high-perfomring web applications. Let&apos;s diccuss you next
+            project.
           </p>
         </div>
 
@@ -137,7 +138,7 @@ export const HeroSection = () => {
           </button>
           <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
             <span className="">👋</span>
-            <span className="font-semibold">Let's Connect</span>
+            <span className="font-semibold">Let&apos;s Connect</span>
           </button>
         </div>
       </div>
